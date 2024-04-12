37e702671b785687b65a5bf940e111b5322e5448
siimpildre <113007373+siimpildre@users.noreply.github.com>
2024-04-12 13:33:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import { GameBoard } from "./src/GameBoard.js";
+import { Snake } from "./src/Snake.js";
+import { Food } from "./src/Food.js";
+
+const boardSize = 20;
+
+const gameBoard = new GameBoard( boardSize );
+const snake = new Snake( boardSize );
+let food = new Food( boardSize, snake.getCoordinates() );
+
+const intervalId = setInterval( () => {
+
+    const newHead = snake.calculateNewHead(boardSize);
+
+    const snakeCoordinates = snake.getCoordinates();
+    const foodCoordinates = food.y + '-' + food.x
+
+    if ( snakeCoordinates.includes(newHead) ) {
+        gameBoard.gameOver();
+        clearInterval(intervalId);
+    }
+    else {
+        snake.unshift(newHead);
+    }
+
+    if( foodCoordinates == snakeCoordinates[0] ) {
+        food = new Food( boardSize, snakeCoordinates) ;
+    }
+    else {
+        snake.pop();
+    }
+    gameBoard.draw( snakeCoordinates, food );
+
+}, 200);
---
+++ src/Food.js
@@ -0,0 +1,30 @@
+class Food {
+
+    // &#x1F34e;
+    emojis = ['🍎']
+
+    y;
+    x;
+    emoji;
+
+    constructor ( boardSize, snakeCoordinates ) {
+
+        let c;
+
+        do {
+            this.y = Math.floor(Math.random() * boardSize);
+            this.x = Math.floor(Math.random() * boardSize);
+            c = this.y + '-' + this.x;
+        } while ( snakeCoordinates.includes(c) );
+
+
+        const emojiIndex = Math.floor(Math.random() * this.emojis.length);
+        this.emoji = this.emojis[emojiIndex];
+    }
+
+        getEmoji() {
+          return this.emoji;
+        }
+}
+
+export { Food }
